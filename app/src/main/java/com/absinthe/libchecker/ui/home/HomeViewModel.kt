e21987de19d26738c545ce7fe6d561f15b490221
zhaobozhen <zhaobozhen2025@gmail.com>
2020-03-19 11:20:48 +0800

    Initial commit

---
+++ app/src/main/java/com/absinthe/libchecker/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.absinthe.libchecker.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
