127747ad878e4b294484fede44742379a420660d
zhaobozhen <zhaobozhen2025@gmail.com>
2020-05-29 19:59:38 +0800

    Update LibReferenceAdapter.kt

--- app/src/main/kotlin/com/absinthe/libchecker/recyclerview/LibReferenceAdapter.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/recyclerview/LibReferenceAdapter.kt
@@ -13,41 +13,52 @@ class LibReferenceAdapter :
     override fun convert(holder: BaseViewHolder, item: LibReference) {
         holder.setText(R.id.tv_lib_name, item.libName)
 
+        var found = false
         when (item.type) {
             TYPE_NATIVE -> {
                 NativeLibMap.MAP[item.libName]?.let {
                     holder.setImageResource(R.id.iv_icon, it.iconRes)
                     holder.setText(R.id.tv_label_name, it.name)
+                    found = true
                 }
             }
             TYPE_SERVICE -> {
                 ServiceLibMap.MAP[item.libName]?.let {
                     holder.setImageResource(R.id.iv_icon, it.iconRes)
                     holder.setText(R.id.tv_label_name, it.name)
+                    found = true
                 }
             }
             TYPE_ACTIVITY -> {
                 ActivityLibMap.MAP[item.libName]?.let {
                     holder.setImageResource(R.id.iv_icon, it.iconRes)
                     holder.setText(R.id.tv_label_name, it.name)
+                    found = true
                 }
             }
             TYPE_BROADCAST_RECEIVER -> {
                 ReceiverLibMap.MAP[item.libName]?.let {
                     holder.setImageResource(R.id.iv_icon, it.iconRes)
                     holder.setText(R.id.tv_label_name, it.name)
+                    found = true
                 }
             }
             TYPE_CONTENT_PROVIDER -> {
                 ProviderLibMap.MAP[item.libName]?.let {
                     holder.setImageResource(R.id.iv_icon, it.iconRes)
                     holder.setText(R.id.tv_label_name, it.name)
+                    found = true
                 }
             }
             else -> {
                 holder.setImageResource(R.id.iv_icon, R.drawable.ic_question)
                 holder.setText(R.id.tv_label_name, R.string.not_marked_lib)
             }
+        }
+
+        if (!found) {
+            holder.setImageResource(R.id.iv_icon, R.drawable.ic_question)
+            holder.setText(R.id.tv_label_name, R.string.not_marked_lib)
         }
 
         holder.setText(R.id.tv_count, item.referredCount.toString())
