05b63ac310194e090e8b9ebd565472afc2c3848d
zhaobozhen <zhaobozhen2025@gmail.com>
2020-05-29 14:49:40 +0800

    AGP to 4.0.0.

--- app/src/main/kotlin/com/absinthe/libchecker/constant/GlobalValues.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/constant/GlobalValues.kt
@@ -1,14 +1,11 @@
 package com.absinthe.libchecker.constant
 
 import android.content.Context
-import android.content.ContextWrapper
 import android.content.SharedPreferences
-import android.os.Build
 import androidx.lifecycle.MutableLiveData
 import com.absinthe.libchecker.BuildConfig
 import com.absinthe.libchecker.api.bean.Configuration
 import com.absinthe.libchecker.ui.fragment.applist.MODE_SORT_BY_SIZE
-import com.absinthe.libchecker.utils.EmptySharedPreferencesImpl
 
 const val SP_NAME = "${BuildConfig.APPLICATION_ID}_preferences"
 
@@ -16,7 +13,7 @@ object GlobalValues {
 
     private var sPreferences: SharedPreferences? = null
 
-    fun getPreferences(): SharedPreferences {
+    private fun getPreferences(): SharedPreferences {
         return sPreferences!!
     }
 
@@ -40,7 +37,7 @@ object GlobalValues {
 
     fun init(context: Context) {
         if (sPreferences == null) {
-            sPreferences = getSettingsStorageContext(context)
+            sPreferences = context
                 .getSharedPreferences(SP_NAME, Context.MODE_PRIVATE)
         }
 
@@ -59,24 +56,5 @@ object GlobalValues {
 
         repo = getPreferences().getString(Constants.PREF_RULES_REPO, Constants.REPO_GITHUB)
             ?: Constants.REPO_GITHUB
-    }
-
-    private fun getSettingsStorageContext(context: Context): Context {
-        var storageContext: Context = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            context.createDeviceProtectedStorageContext()
-        } else {
-            context
-        }
-        storageContext = object : ContextWrapper(storageContext) {
-            override fun getSharedPreferences(name: String, mode: Int): SharedPreferences {
-                return try {
-                    super.getSharedPreferences(name, mode)
-                } catch (e: IllegalStateException) {
-                    // SharedPreferences in credential encrypted storage are not available until after user is unlocked
-                    EmptySharedPreferencesImpl()
-                }
-            }
-        }
-        return storageContext
     }
 }
