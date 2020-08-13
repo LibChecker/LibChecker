0975342ce043d1696ad1c635956d4211e8642875
Bozhen Zhao <zhaobozhen2025@gmail.com>
2020-08-13 15:57:41 +0800

    Bump me lib to 1.0.1.

--- app/src/main/kotlin/com/absinthe/libchecker/constant/URLManager.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/constant/URLManager.kt
@@ -4,10 +4,10 @@ import com.absinthe.libchecker.BuildConfig
 import com.absinthe.libraries.Absinthe
 
 object URLManager {
-    const val MARKET_PAGE = "market://details?id=${BuildConfig.APPLICATION_ID}"
+    const val MARKET_PAGE = "${Absinthe.MARKET_DETAIL_SCHEME}${BuildConfig.APPLICATION_ID}"
     const val COOLAPK_APP_PAGE = "coolmarket://apk/${BuildConfig.APPLICATION_ID}"
 
-    const val COOLAPK_HOME_PAGE = "coolmarket://u/482045"
+    const val COOLAPK_HOME_PAGE = Absinthe.COOLAPK_HOME_PAGE
     const val GITHUB_PAGE = Absinthe.GITHUB_HOME_PAGE
 
     const val GITHUB_REPO_PAGE = "https://github.com/zhaobozhen/LibChecker"
