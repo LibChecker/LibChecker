bb2fa27a5330c32e7071b8c74e6fce85d8fbdbc3
Bozhen Zhao <zhaobozhen2025@gmail.com>
2020-08-20 22:00:00 +0800

    Fix contour crash on kotlin 1.4.

--- app/src/main/kotlin/com/absinthe/libchecker/view/dialogfragment/LibThresholdDialogFragment.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/view/dialogfragment/LibThresholdDialogFragment.kt
@@ -7,25 +7,23 @@ import androidx.fragment.app.DialogFragment
 import com.absinthe.libchecker.R
 import com.absinthe.libchecker.constant.Constants
 import com.absinthe.libchecker.constant.GlobalValues
+import com.absinthe.libchecker.databinding.LayoutLibRefThresholdBinding
 import com.absinthe.libchecker.utils.SPUtils
-import com.absinthe.libchecker.view.settings.LibReferenceThresholdView
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.analytics.EventProperties
 
 class LibThresholdDialogFragment : DialogFragment() {
 
-    private val dialogView by lazy {
-        LibReferenceThresholdView(
-            requireContext()
-        )
-    }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val binding = LayoutLibRefThresholdBinding.inflate(layoutInflater)
+        binding.slider.value = GlobalValues.libReferenceThreshold.value?.toFloat() ?: 1f
+
         return AlertDialog.Builder(requireContext())
-            .setView(dialogView)
+            .setView(binding.root)
             .setTitle(R.string.lib_ref_threshold)
             .setPositiveButton(android.R.string.ok) { _, _ ->
-                val threshold = dialogView.slider.value.toInt()
+                val threshold = binding.slider.value.toInt()
                 GlobalValues.libReferenceThreshold.value = threshold
                 SPUtils.putInt(Constants.PREF_LIB_REF_THRESHOLD, threshold)
                 Analytics.trackEvent(Constants.Event.SETTINGS, EventProperties().set("PREF_LIB_REF_THRESHOLD", threshold.toLong()))
