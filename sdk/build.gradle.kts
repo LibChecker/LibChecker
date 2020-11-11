079ac1d37f1dd1bf592a498273722f81ad8663c7
Bozhen Zhao <zhaobozhen2025@gmail.com>
2020-11-11 12:47:53 +0800

    Add :sdk module.

---
+++ sdk/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion = "30.0.2"
+
+    signingConfigs {
+        register("release") {
+            storeFile = file("android-key.jks")
+            storePassword = System.getenv("KSTOREPWD")
+            keyAlias = System.getenv("KEYALIAS")
+            keyPassword = System.getenv("KEYPWD")
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.absinthe.libchecker"
+        minSdkVersion(23)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isZipAlignEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    // To inline the bytecode built with JVM target 1.8 into
+    // bytecode that is being built with JVM target 1.6. (e.g. navArgs)
+
+    sourceSets {
+        getByName("main").java.apply {
+            srcDirs("src/main/kotlin")
+            srcDirs("src/main/java")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn", "-XXLanguage:+InlineClasses")
+    }
+
+    packagingOptions {
+        exclude("META-INF/atomicfu.kotlin_module")
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.0")
+
+    implementation("com.absinthe.libraries.me:me:1.0.6")
+    implementation("com.absinthe.libraries.utils:utils:1.0.9")
+
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.core:core-ktx:1.5.0-alpha04")
+
+    testImplementation("junit:junit:4.13.1")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.5")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
