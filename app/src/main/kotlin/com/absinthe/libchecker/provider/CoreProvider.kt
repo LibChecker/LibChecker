0281650d9d1f8bff0a92a5557d6d21c8fb25970b
Bozhen Zhao <zhaobozhen2025@gmail.com>
2020-11-11 12:31:41 +0800

    Create CoreProvider.

---
+++ app/src/main/kotlin/com/absinthe/libchecker/provider/CoreProvider.kt
@@ -0,0 +1,33 @@
+package com.absinthe.libchecker.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class CoreProvider : ContentProvider() {
+
+    override fun onCreate(): Boolean {
+        return false
+    }
+
+    override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
+        return 0
+    }
+}
