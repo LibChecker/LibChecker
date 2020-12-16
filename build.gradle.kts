d956dbe3927ec39b8386d0b23f0b52eb4a7fb396
Absinthe <zhaobozhen2025@gmail.com>
2020-12-16 22:47:56 +0800

    ci: AGP to 4.1.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.android.tools.build:gradle:4.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
 
         // NOTE: Do not place your application dependencies here; they belong
