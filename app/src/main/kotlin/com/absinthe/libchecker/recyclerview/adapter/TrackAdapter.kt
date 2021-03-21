80164824c59d1ec44a48a63c582796dc3a1bb9e6
Absinthe <zhaobozhen2025@gmail.com>
2021-03-21 16:39:59 +0800

    refactor: Create TrackItemView programmatically.

--- app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/TrackAdapter.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/TrackAdapter.kt
@@ -1,36 +1,62 @@
 package com.absinthe.libchecker.recyclerview.adapter
 
 import android.content.pm.PackageManager
-import android.widget.ImageView
-import coil.load
+import android.view.ContextThemeWrapper
+import android.view.ViewGroup
+import androidx.lifecycle.LifecycleCoroutineScope
 import com.absinthe.libchecker.R
 import com.absinthe.libchecker.bean.TrackListItem
+import com.absinthe.libchecker.extensions.getDimensionPixelSize
+import com.absinthe.libchecker.utils.AppIconCache
 import com.absinthe.libchecker.utils.PackageUtils
+import com.absinthe.libchecker.view.snapshot.TrackItemView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
-import com.google.android.material.switchmaterial.SwitchMaterial
-import me.zhanghai.android.appiconloader.AppIconLoader
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class TrackAdapter :BaseQuickAdapter<TrackListItem, BaseViewHolder>(R.layout.item_track) {
+class TrackAdapter(val lifecycleScope: LifecycleCoroutineScope) : BaseQuickAdapter<TrackListItem, BaseViewHolder>(0) {
 
-    private val iconLoader by lazy { AppIconLoader(context.resources.getDimensionPixelSize(R.dimen.app_icon_size), false,context) }
+    private var loadIconJob: Job? = null
 
     init {
-        addChildClickViewIds(R.id.track_switch)
+        addChildClickViewIds(android.R.id.toggle)
+    }
+
+    override fun onCreateDefViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return createBaseViewHolder(
+            TrackItemView(ContextThemeWrapper(context, R.style.AppListMaterialCard)).apply {
+                layoutParams = ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT).also {
+                    val margin = context.getDimensionPixelSize(R.dimen.main_card_margin)
+                    it.setMargins(margin, margin, margin, margin)
+                }
+            }
+        )
     }
 
     override fun convert(holder: BaseViewHolder, item: TrackListItem) {
-        val icon = try {
-            iconLoader.loadIcon(PackageUtils.getPackageInfo(item.packageName, PackageManager.GET_META_DATA).applicationInfo)
-        } catch (e: PackageManager.NameNotFoundException) {
-            null
-        }
-        holder.getView<ImageView>(R.id.iv_icon).load(icon)
-        holder.setText(R.id.tv_app_name, item.label)
-        holder.setText(R.id.tv_package_name, item.packageName)
-        holder.getView<SwitchMaterial>(R.id.track_switch).apply {
-            isChecked = item.switchState
+        (holder.itemView as TrackItemView).container.apply {
+            lifecycleScope.launch(Dispatchers.IO) {
+                try {
+                    val ai = PackageUtils.getPackageInfo(item.packageName).applicationInfo
+                    val drawable = ai.loadIcon(context.packageManager)
+                    icon.post { icon.setImageDrawable(drawable) }
+                    loadIconJob = AppIconCache.loadIconBitmapAsync(context, ai, ai.uid / 100000, icon)
+                } catch (e: PackageManager.NameNotFoundException) {
+                    Timber.e(e)
+                }
+            }
+            appName.text = item.label
+            packageName.text = item.packageName
+            switch.isChecked = item.switchState
         }
     }
 
+    fun release() {
+        if (loadIconJob?.isActive == true) {
+            loadIconJob?.cancel()
+        }
+    }
 }
--- app/src/main/kotlin/com/absinthe/libchecker/view/snapshot/TrackItemView.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/view/snapshot/TrackItemView.kt
@@ -1,10 +1,10 @@
-package com.absinthe.libchecker.view.applist
+package com.absinthe.libchecker.view.snapshot
 
 import android.content.Context
-import android.text.TextUtils
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
@@ -13,10 +13,11 @@ import com.absinthe.libchecker.R
 import com.absinthe.libchecker.extensions.getDimensionPixelSize
 import com.absinthe.libchecker.view.AViewGroup
 import com.google.android.material.card.MaterialCardView
+import com.google.android.material.switchmaterial.SwitchMaterial
 
-class AppItemView(context: Context) : MaterialCardView(context) {
+class TrackItemView(context: Context) : MaterialCardView(context) {
 
-    val container = AppItemContainerView(context).apply {
+    val container = TrackItemContainerView(context).apply {
         val padding = context.getDimensionPixelSize(R.dimen.main_card_padding)
         setPadding(padding, padding, padding, padding)
     }
@@ -25,11 +26,11 @@ class AppItemView(context: Context) : MaterialCardView(context) {
         addView(container)
     }
 
-    class AppItemContainerView(context: Context) : AViewGroup(context) {
+    class TrackItemContainerView(context: Context) : AViewGroup(context) {
 
         val icon = AppCompatImageView(context).apply {
             val iconSize = context.getDimensionPixelSize(R.dimen.app_icon_size)
-            layoutParams = LayoutParams(iconSize, iconSize)
+            layoutParams = FrameLayout.LayoutParams(iconSize, iconSize)
             addView(this)
         }
 
@@ -39,8 +40,6 @@ class AppItemView(context: Context) : MaterialCardView(context) {
             }
             setTextColor(ContextCompat.getColor(context, R.color.textNormal))
             setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
-            maxLines = 1
-            ellipsize = TextUtils.TruncateAt.END
             addView(this)
         }
 
@@ -48,46 +47,31 @@ class AppItemView(context: Context) : MaterialCardView(context) {
             layoutParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             setTextColor(ContextCompat.getColor(context, R.color.textNormal))
             setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
-            maxLines = 1
-            ellipsize = TextUtils.TruncateAt.END
             addView(this)
         }
 
-        val versionInfo = AppCompatTextView(ContextThemeWrapper(context, R.style.TextView_SansSerifCondensed)).apply {
-            layoutParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            setTextColor(ContextCompat.getColor(context, android.R.color.darker_gray))
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
-            maxLines = 1
-            ellipsize = TextUtils.TruncateAt.END
-            addView(this)
-        }
-
-        val abiInfo = AppCompatTextView(ContextThemeWrapper(context, R.style.TextView_SansSerifCondensedMedium)).apply {
-            layoutParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            setTextColor(ContextCompat.getColor(context, android.R.color.darker_gray))
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
-            maxLines = 1
-            ellipsize = TextUtils.TruncateAt.END
+        val switch = SwitchMaterial(context).apply {
+            id = android.R.id.toggle
+            layoutParams = LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             addView(this)
         }
 
         override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec)
             icon.autoMeasure()
-            val textWidth = measuredWidth - paddingStart - paddingEnd - icon.measuredWidth - appName.marginStart
+            switch.autoMeasure()
+            val textWidth = measuredWidth - paddingStart - paddingEnd - icon.measuredWidth - appName.marginStart - switch.measuredWidth
             appName.measure(textWidth.toExactlyMeasureSpec(), appName.defaultHeightMeasureSpec(this))
-            packageName.measure(textWidth.toExactlyMeasureSpec(), packageName.defaultHeightMeasureSpec(this))
-            versionInfo.measure(textWidth.toExactlyMeasureSpec(), versionInfo.defaultHeightMeasureSpec(this))
-            abiInfo.measure(textWidth.toExactlyMeasureSpec(), abiInfo.defaultHeightMeasureSpec(this))
-            setMeasuredDimension(measuredWidth, paddingTop + appName.measuredHeight + packageName.measuredHeight + versionInfo.measuredHeight + abiInfo.measuredHeight + paddingBottom)
+            packageName.measure(textWidth.toExactlyMeasureSpec(), appName.defaultHeightMeasureSpec(this))
+            setMeasuredDimension(measuredWidth, (paddingTop + appName.measuredHeight + packageName.measuredHeight + paddingBottom)
+                .coerceAtLeast(icon.measuredHeight + paddingTop + paddingBottom))
         }
 
         override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
             icon.layout(paddingStart, (measuredHeight - icon.measuredHeight) / 2)
+            switch.layout(paddingEnd, (measuredHeight - switch.measuredHeight) / 2, fromRight = true)
             appName.layout(icon.right + appName.marginStart, paddingTop)
             packageName.layout(appName.left, appName.bottom)
-            versionInfo.layout(appName.left, packageName.bottom)
-            abiInfo.layout(appName.left, versionInfo.bottom)
         }
     }
 }
