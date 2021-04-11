0a8b3bacc57b395c69f1423443279daae061a47f
Absinthe <zhaobozhen2025@gmail.com>
2021-04-11 14:41:39 +0800

    ci: AGP to 4.2.0-rc01 and Gradle to 7.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+        classpath("com.android.tools.build:gradle:4.2.0-rc01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
