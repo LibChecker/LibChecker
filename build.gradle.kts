d0ebeb816f5f77cc3e6595c9ca5ce73fd06d7cd3
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2021-05-23 19:24:26 +0800

    Update HiddenApiBypass to 2.0 and update AGP to beta-02

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta01")
+        classpath("com.android.tools.build:gradle:7.0.0-beta02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
 
         // NOTE: Do not place your application dependencies here; they belong
