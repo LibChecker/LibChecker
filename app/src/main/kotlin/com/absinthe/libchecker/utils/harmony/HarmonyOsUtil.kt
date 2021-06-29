cea0e2b13b30a29f0eab5a461bb7b7d2b4f2f00b
su <suzhaoqiang122@163.com>
2021-06-29 12:59:47 +0800

    add HarmonyOS support (#58)

---
+++ app/src/main/kotlin/com/absinthe/libchecker/utils/harmony/HarmonyOsUtil.kt
@@ -0,0 +1,32 @@
+package com.absinthe.libchecker.utils.harmony
+
+/**
+ * Created by su1216 on 21-6-28.
+ */
+object HarmonyOsUtil {
+
+    private var sHarmonyOs: Boolean? = null
+
+    fun <T> wrapperStub(f: () -> T): T? {
+        return try {
+            f()
+        } catch (e: Throwable) {
+            null
+        }
+    }
+
+    fun isHarmonyOs(): Boolean {
+        if (sHarmonyOs != null) {
+            return sHarmonyOs!!
+        }
+        return try {
+            Class.forName("ohos.app.Application")
+            Class.forName("ohos.system.version.SystemVersion")
+            sHarmonyOs = true
+            sHarmonyOs!!
+        } catch (e: ClassNotFoundException) {
+            sHarmonyOs = false
+            sHarmonyOs!!
+        }
+    }
+}
---
+++ app/src/main/kotlin/com/absinthe/libchecker/utils/harmony/SystemVersionDelegate.kt
@@ -0,0 +1,11 @@
+package com.absinthe.libchecker.utils.harmony
+
+import ohos.system.version.SystemVersion
+
+/**
+ * Created by su1216 on 21-6-28.
+ */
+object SystemVersionDelegate {
+
+    fun getVersion(): String? = HarmonyOsUtil.wrapperStub { SystemVersion.getVersion() }
+}
