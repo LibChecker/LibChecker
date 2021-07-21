e7a77836404a9d3f3838334a00da96e326406533
Goooler <wangzongler@gmail.com>
2021-07-21 12:14:30 +0800

    Bump AGP to 7.0.0-rc01

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath("com.android.tools.build:gradle:7.0.0-rc01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
     }
