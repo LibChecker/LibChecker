42c724ef9d14ea014b15d234f5cee1805d805ec5
Goooler <wangzongler@gmail.com>
2021-07-25 12:58:18 +0800

    Apply kotlinter plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
@@ -9,10 +7,13 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.0-rc01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
+        classpath("org.jmailen.gradle:kotlinter-gradle:3.4.5")
     }
 }
 
 allprojects {
+    apply(plugin = "org.jmailen.kotlinter")
+
     repositories {
         google()
         mavenCentral()
