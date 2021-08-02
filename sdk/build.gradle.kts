a29e1f75b7474410fbc720aded656f8560fb92ae
Goooler <wangzongler@gmail.com>
2021-08-02 18:43:48 +0800

    Remove leakCanary in sdk module

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
   implementation("androidx.core:core-ktx:1.6.0")
 
   testImplementation("junit:junit:4.13.2")
-  debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
   androidTestImplementation("androidx.test.ext:junit:1.1.3")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
