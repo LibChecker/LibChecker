f62fd8d6774bd56d9871a68925d5b2866bb7e771
Goooler <wangzongler@gmail.com>
2021-08-02 18:57:46 +0800

    Apply extra.gradle.kts in root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,28 @@
 import org.jmailen.gradle.kotlinter.KotlinterExtension
 
 buildscript {
+  apply("gradle/extra.gradle.kts")
+
   repositories {
     google()
     gradlePluginPortal()
   }
+
   dependencies {
-    classpath("com.android.tools.build:gradle:7.0.0")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    classpath(rootProject.extra["androidPlugin"].toString())
+    classpath(rootProject.extra["kotlinPlugin"].toString())
     classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
     classpath("org.jmailen.gradle:kotlinter-gradle:3.4.5")
   }
 }
 
 allprojects {
+  apply("$rootDir/gradle/extra.gradle.kts")
+
   apply(plugin = "org.jmailen.kotlinter")
 
   configure<KotlinterExtension> {
     indentSize = 2
-  }
-
-  repositories {
-    google()
-    mavenCentral()
-    maven("https://jitpack.io")
   }
 }
 
