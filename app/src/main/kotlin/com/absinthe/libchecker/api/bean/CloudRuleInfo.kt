75fcc37d994749399f49087ab97371255c5306e6
Absinthe <zhaobozhen2025@gmail.com>
2021-08-03 21:59:32 +0800

    Remove unused param.

--- app/src/main/kotlin/com/absinthe/libchecker/api/bean/CloudRuleInfo.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/api/bean/CloudRuleInfo.kt
@@ -6,6 +6,5 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class CloudRuleInfo(
   @field:Json(name = "version") val version: Int,
-  @field:Json(name = "count") val count: Int,
-  @field:Json(name = "bundles") val bundles: Int
+  @field:Json(name = "count") val count: Int
 )
