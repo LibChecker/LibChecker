d3373e242af99abcf1008e080a5d3c090994c516
Goooler <wangzongler@gmail.com>
2021-08-25 17:30:02 +0800

    Bump kotlin to 1.5.30 (#143)

--- gradle/extra.gradle.kts
+++ gradle/extra.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.run {
   set("androidPlugin", "com.android.tools.build:gradle:7.0.1")
-  set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+  set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
 }
 
 repositories {
