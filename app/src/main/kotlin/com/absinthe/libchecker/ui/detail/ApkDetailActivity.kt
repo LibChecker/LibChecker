bd9c9048b31ed61b35a55c7886cb20073e82aca4
Absinthe <zhaobozhen2025@gmail.com>
2021-12-11 09:45:04 +0800

    Fix format.

--- app/src/main/kotlin/com/absinthe/libchecker/ui/detail/ApkDetailActivity.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/ui/detail/ApkDetailActivity.kt
@@ -137,228 +137,224 @@ class ApkDetailActivity : BaseActivity<ActivityAppDetailBinding>(), IDetailConta
   }
 
   private fun initDetails(path: String) {
-    (
-      packageManager.getPackageArchiveInfo(path, 0) ?: run {
-        Timber.e("PackageInfo is null")
-        finish()
-        return
-      }
-      ).also {
-        it.applicationInfo.sourceDir = path
-        it.applicationInfo.publicSourceDir = path
+    packageManager.getPackageArchiveInfo(path, 0)?.also {
+      it.applicationInfo.sourceDir = path
+      it.applicationInfo.publicSourceDir = path
 
-        supportActionBar?.title = null
-        binding.apply {
-          try {
-            collapsingToolbar.title = it.applicationInfo.loadLabel(packageManager)
-            headerLayout.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
-              override fun onStateChanged(appBarLayout: AppBarLayout, state: State) {
-                collapsingToolbar.isTitleEnabled = state == State.COLLAPSED
-              }
-            })
-            val appIconLoader = AppIconLoader(
-              resources.getDimensionPixelSize(R.dimen.lib_detail_icon_size),
-              false,
-              this@ApkDetailActivity
+      supportActionBar?.title = null
+      binding.apply {
+        try {
+          collapsingToolbar.title = it.applicationInfo.loadLabel(packageManager)
+          headerLayout.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
+            override fun onStateChanged(appBarLayout: AppBarLayout, state: State) {
+              collapsingToolbar.isTitleEnabled = state == State.COLLAPSED
+            }
+          })
+          val appIconLoader = AppIconLoader(
+            resources.getDimensionPixelSize(R.dimen.lib_detail_icon_size),
+            false,
+            this@ApkDetailActivity
+          )
+          detailsTitle.apply {
+            iconView.load(appIconLoader.loadIcon(it.applicationInfo))
+            appNameView.apply {
+              text = it.applicationInfo.loadLabel(packageManager)
+              setLongClickCopiedToClipboard(text)
+            }
+            packageNameView.apply {
+              text = it.packageName
+              setLongClickCopiedToClipboard(text)
+            }
+            versionInfoView.apply {
+              text = PackageUtils.getVersionString(it)
+              setLongClickCopiedToClipboard(text)
+            }
+          }
+
+          val extraInfo = SpannableStringBuilder()
+          val file = File(it.applicationInfo.sourceDir)
+          val demands = ManifestReader.getManifestProperties(
+            file,
+            arrayOf(
+              PackageUtils.use32bitAbiString,
+              PackageUtils.multiArchString,
+              PackageUtils.overlayString
             )
-            detailsTitle.apply {
-              iconView.load(appIconLoader.loadIcon(it.applicationInfo))
-              appNameView.apply {
-                text = it.applicationInfo.loadLabel(packageManager)
-                setLongClickCopiedToClipboard(text)
+          )
+          val overlay = demands[PackageUtils.overlayString] as? Boolean ?: false
+          val abiSet = PackageUtils.getAbiSet(
+            file,
+            it.applicationInfo,
+            isApk = true,
+            overlay = overlay,
+            ignoreArch = true
+          )
+          val abi = PackageUtils.getAbi(it.applicationInfo, true)
+
+          if (abiSet.isNotEmpty() && !abiSet.contains(Constants.OVERLAY) && !abiSet.contains(
+              Constants.ERROR
+            )
+          ) {
+            val spanStringBuilder = SpannableStringBuilder()
+            var spanString: SpannableString
+            var firstLoop = true
+            var itemCount = 0
+            abiSet.forEach { eachAbi ->
+              itemCount++
+              if (firstLoop) {
+                firstLoop = false
               }
-              packageNameView.apply {
-                text = it.packageName
-                setLongClickCopiedToClipboard(text)
+              PackageUtils.getAbiString(
+                this@ApkDetailActivity,
+                eachAbi,
+                false
+              ).let { str ->
+                spanString = SpannableString("  $str")
               }
-              versionInfoView.apply {
-                text = PackageUtils.getVersionString(it)
-                setLongClickCopiedToClipboard(text)
+              PackageUtils.getAbiBadgeResource(eachAbi).getDrawable(this@ApkDetailActivity)
+                ?.mutate()?.let { drawable ->
+                  drawable.setBounds(
+                    0,
+                    0,
+                    drawable.intrinsicWidth,
+                    drawable.intrinsicHeight
+                  )
+                  val span = CenterAlignImageSpan(drawable)
+                  spanString.setSpan(span, 0, 1, ImageSpan.ALIGN_BOTTOM)
+                }
+              spanStringBuilder.append(spanString)
+              if (itemCount < abiSet.size) {
+                spanStringBuilder.append(", ")
+              }
+              if (itemCount == 3 && abiSet.size > 3 && isOrientationPortrait) {
+                spanStringBuilder.appendLine()
               }
             }
+            extraInfo.append(spanStringBuilder).appendLine()
+          }
 
-            val extraInfo = SpannableStringBuilder()
-            val file = File(it.applicationInfo.sourceDir)
-            val demands = ManifestReader.getManifestProperties(
-              file,
-              arrayOf(
-                PackageUtils.use32bitAbiString,
-                PackageUtils.multiArchString,
-                PackageUtils.overlayString
-              )
-            )
-            val overlay = demands[PackageUtils.overlayString] as? Boolean ?: false
-            val abiSet = PackageUtils.getAbiSet(
-              file,
-              it.applicationInfo,
-              isApk = true,
-              overlay = overlay,
-              ignoreArch = true
-            )
-            val abi = PackageUtils.getAbi(it.applicationInfo, true)
-
-            if (abiSet.isNotEmpty() && !abiSet.contains(Constants.OVERLAY) && !abiSet.contains(
-                Constants.ERROR
-              )
-            ) {
-              val spanStringBuilder = SpannableStringBuilder()
-              var spanString: SpannableString
-              var firstLoop = true
-              var itemCount = 0
-              abiSet.forEach { eachAbi ->
-                itemCount++
-                if (firstLoop) {
-                  firstLoop = false
-                }
-                PackageUtils.getAbiString(
-                  this@ApkDetailActivity,
-                  eachAbi,
-                  false
-                ).let { str ->
-                  spanString = SpannableString("  $str")
-                }
-                PackageUtils.getAbiBadgeResource(eachAbi).getDrawable(this@ApkDetailActivity)
-                  ?.mutate()?.let { drawable ->
-                    drawable.setBounds(
-                      0,
-                      0,
-                      drawable.intrinsicWidth,
-                      drawable.intrinsicHeight
-                    )
-                    val span = CenterAlignImageSpan(drawable)
-                    spanString.setSpan(span, 0, 1, ImageSpan.ALIGN_BOTTOM)
-                  }
-                spanStringBuilder.append(spanString)
-                if (itemCount < abiSet.size) {
-                  spanStringBuilder.append(", ")
-                }
-                if (itemCount == 3 && abiSet.size > 3 && isOrientationPortrait) {
-                  spanStringBuilder.appendLine()
-                }
-              }
-              extraInfo.append(spanStringBuilder).appendLine()
-            }
-
-            val advanced = when (abi) {
-              Constants.ERROR -> getString(R.string.cannot_read)
-              Constants.OVERLAY -> Constants.OVERLAY_STRING
-              else -> ""
-            }
-            extraInfo.apply {
-              append(advanced)
-              if (abi >= Constants.MULTI_ARCH) {
-                if (advanced.isNotEmpty()) {
-                  append(", ")
-                }
-                append(getString(R.string.multiArch))
+          val advanced = when (abi) {
+            Constants.ERROR -> getString(R.string.cannot_read)
+            Constants.OVERLAY -> Constants.OVERLAY_STRING
+            else -> ""
+          }
+          extraInfo.apply {
+            append(advanced)
+            if (abi >= Constants.MULTI_ARCH) {
+              if (advanced.isNotEmpty()) {
                 append(", ")
               }
-              append(PackageUtils.getTargetApiString(it))
-              append(", ").append(PackageUtils.getMinSdkVersion(it))
-              it.sharedUserId?.let { sharedUid ->
-                appendLine().append("sharedUserId = $sharedUid")
-              }
+              append(getString(R.string.multiArch))
+              append(", ")
             }
-            detailsTitle.extraInfoView.text = extraInfo
-          } catch (e: Exception) {
-            Timber.e(e)
-            finish()
-          }
-
-          ibSort.setOnClickListener {
-            lifecycleScope.launch {
-              detailFragmentManager.sortAll()
-              viewModel.sortMode = if (viewModel.sortMode == MODE_SORT_BY_LIB) {
-                MODE_SORT_BY_SIZE
-              } else {
-                MODE_SORT_BY_LIB
-              }
-              detailFragmentManager.changeSortMode(viewModel.sortMode)
+            append(PackageUtils.getTargetApiString(it))
+            append(", ").append(PackageUtils.getMinSdkVersion(it))
+            it.sharedUserId?.let { sharedUid ->
+              appendLine().append("sharedUserId = $sharedUid")
             }
           }
+          detailsTitle.extraInfoView.text = extraInfo
+        } catch (e: Exception) {
+          Timber.e(e)
+          finish()
         }
 
-        val types = mutableListOf(
-          NATIVE, SERVICE, ACTIVITY, RECEIVER, PROVIDER, PERMISSION, METADATA, DEX
-        )
-        val tabTitles = mutableListOf(
-          getText(R.string.ref_category_native),
-          getText(R.string.ref_category_service),
-          getText(R.string.ref_category_activity),
-          getText(R.string.ref_category_br),
-          getText(R.string.ref_category_cp),
-          getText(R.string.ref_category_perm),
-          getText(R.string.ref_category_metadata),
-          getText(R.string.ref_category_dex)
-        )
-        lifecycleScope.launch(Dispatchers.IO) {
-          try {
-            if (PackageUtils.getStaticLibs(PackageUtils.getPackageInfo(it.packageName))
-                .isNotEmpty()
-            ) {
-              withContext(Dispatchers.Main) {
-                types.add(1, STATIC)
-                tabTitles.add(1, getText(R.string.ref_category_static))
-                binding.tabLayout.addTab(
-                  binding.tabLayout.newTab()
-                    .also { tab -> tab.text = getText(R.string.ref_category_static) },
-                  1
-                )
-              }
+        ibSort.setOnClickListener {
+          lifecycleScope.launch {
+            detailFragmentManager.sortAll()
+            viewModel.sortMode = if (viewModel.sortMode == MODE_SORT_BY_LIB) {
+              MODE_SORT_BY_SIZE
+            } else {
+              MODE_SORT_BY_LIB
             }
-          } catch (e: PackageManager.NameNotFoundException) {
-            Timber.e(e)
-          }
-        }
-
-        binding.viewpager.adapter = object : FragmentStateAdapter(this) {
-          override fun getItemCount(): Int {
-            return types.size
-          }
-
-          override fun createFragment(position: Int): Fragment {
-            return when (position) {
-              types.indexOf(NATIVE) -> NativeAnalysisFragment.newInstance(path)
-              types.indexOf(STATIC) -> StaticAnalysisFragment.newInstance(path)
-              types.indexOf(PERMISSION) -> PermissionAnalysisFragment.newInstance(path)
-              types.indexOf(METADATA) -> MetaDataAnalysisFragment.newInstance(path)
-              types.indexOf(DEX) -> DexAnalysisFragment.newInstance(path)
-              else -> ComponentsAnalysisFragment.newInstance(types[position])
-            }
-          }
-        }
-        binding.tabLayout.apply {
-          tabTitles.forEach {
-            addTab(newTab().apply { text = it })
-          }
-          addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-              val count = viewModel.itemsCountList[types[tab.position]]
-              if (detailFragmentManager.currentItemsCount != count) {
-                binding.tsComponentCount.setText(count.toString())
-                detailFragmentManager.currentItemsCount = count
-              }
-              detailFragmentManager.selectedPosition = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-          })
-        }
-
-        val mediator = TabLayoutMediator(binding.tabLayout, binding.viewpager) { tab, position ->
-          tab.text = tabTitles[position]
-        }
-        mediator.attach()
-
-        viewModel.itemsCountLiveData.observe(this) { locatedCount ->
-          if (detailFragmentManager.currentItemsCount != locatedCount.count && types[binding.tabLayout.selectedTabPosition] == locatedCount.locate) {
-            binding.tsComponentCount.setText(locatedCount.count.toString())
-            detailFragmentManager.currentItemsCount = locatedCount.count
+            detailFragmentManager.changeSortMode(viewModel.sortMode)
           }
         }
       }
+
+      val types = mutableListOf(
+        NATIVE, SERVICE, ACTIVITY, RECEIVER, PROVIDER, PERMISSION, METADATA, DEX
+      )
+      val tabTitles = mutableListOf(
+        getText(R.string.ref_category_native),
+        getText(R.string.ref_category_service),
+        getText(R.string.ref_category_activity),
+        getText(R.string.ref_category_br),
+        getText(R.string.ref_category_cp),
+        getText(R.string.ref_category_perm),
+        getText(R.string.ref_category_metadata),
+        getText(R.string.ref_category_dex)
+      )
+      lifecycleScope.launch(Dispatchers.IO) {
+        try {
+          if (PackageUtils.getStaticLibs(PackageUtils.getPackageInfo(it.packageName)).isNotEmpty()) {
+            withContext(Dispatchers.Main) {
+              types.add(1, STATIC)
+              tabTitles.add(1, getText(R.string.ref_category_static))
+              binding.tabLayout.addTab(
+                binding.tabLayout.newTab()
+                  .also { tab -> tab.text = getText(R.string.ref_category_static) },
+                1
+              )
+            }
+          }
+        } catch (e: PackageManager.NameNotFoundException) {
+          Timber.e(e)
+        }
+      }
+
+      binding.viewpager.adapter = object : FragmentStateAdapter(this) {
+        override fun getItemCount(): Int {
+          return types.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+          return when (position) {
+            types.indexOf(NATIVE) -> NativeAnalysisFragment.newInstance(path)
+            types.indexOf(STATIC) -> StaticAnalysisFragment.newInstance(path)
+            types.indexOf(PERMISSION) -> PermissionAnalysisFragment.newInstance(path)
+            types.indexOf(METADATA) -> MetaDataAnalysisFragment.newInstance(path)
+            types.indexOf(DEX) -> DexAnalysisFragment.newInstance(path)
+            else -> ComponentsAnalysisFragment.newInstance(types[position])
+          }
+        }
+      }
+      binding.tabLayout.apply {
+        tabTitles.forEach {
+          addTab(newTab().apply { text = it })
+        }
+        addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+          override fun onTabSelected(tab: TabLayout.Tab) {
+            val count = viewModel.itemsCountList[types[tab.position]]
+            if (detailFragmentManager.currentItemsCount != count) {
+              binding.tsComponentCount.setText(count.toString())
+              detailFragmentManager.currentItemsCount = count
+            }
+            detailFragmentManager.selectedPosition = tab.position
+          }
+
+          override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+          override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+      }
+
+      val mediator = TabLayoutMediator(binding.tabLayout, binding.viewpager) { tab, position ->
+        tab.text = tabTitles[position]
+      }
+      mediator.attach()
+
+      viewModel.itemsCountLiveData.observe(this) { locatedCount ->
+        if (detailFragmentManager.currentItemsCount != locatedCount.count && types[binding.tabLayout.selectedTabPosition] == locatedCount.locate) {
+          binding.tsComponentCount.setText(locatedCount.count.toString())
+          detailFragmentManager.currentItemsCount = locatedCount.count
+        }
+      }
+    } ?: run {
+      Timber.e("PackageInfo is null")
+      finish()
+      return
+    }
 
     viewModel.initComponentsData(path)
   }
