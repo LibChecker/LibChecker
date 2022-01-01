ba45eda57fcefc462a124d8dfd7a4343a9e5ca34
Absinthe <zhaobozhen2025@gmail.com>
2022-01-01 14:27:10 +0800

    Fix sql.

--- app/src/main/kotlin/com/absinthe/libchecker/database/LCDatabase.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/database/LCDatabase.kt
@@ -288,7 +288,7 @@ abstract class LCDatabase : RoomDatabase() {
     private val MIGRATION_18_19: Migration = object : Migration(18, 19) {
       override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
-          "ALTER TABLE snapshot_table ADD COLUMN packageSize INTEGER NOT NULL"
+          "ALTER TABLE snapshot_table ADD COLUMN packageSize INTEGER NOT NULL DEFAULT 0"
         )
       }
     }
