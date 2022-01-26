ee52ca61811da4d8ed69b65a66a122f461e419f8
Absinthe <zhaobozhen2025@gmail.com>
2022-01-26 11:29:52 +0800

    Bump AGP to 7.1.0.

--- gradle/extra.gradle.kts
+++ gradle/extra.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.run {
-  set("androidPlugin", "com.android.tools.build:gradle:7.0.4")
+  set("androidPlugin", "com.android.tools.build:gradle:7.1.0")
   set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 }
 
