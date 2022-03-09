12ee9903c8ccf0b60f043be3df37f731a522ae65
Goooler <wangzongler@gmail.com>
2022-03-09 10:41:37 +0800

    Adopt PublishedApi (#200)
    * Reformat
    
    * Use PublishedApi in JsonUtil
    
    * Use PublishedApi in ApiManager

--- app/src/main/kotlin/com/absinthe/libchecker/utils/JsonUtil.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/utils/JsonUtil.kt
@@ -4,47 +4,43 @@ package com.absinthe.libchecker.utils
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import java.lang.reflect.Type
+import org.intellij.lang.annotations.Language
 
 object JsonUtil {
+  @PublishedApi
   internal val moshi: Moshi = Moshi.Builder().build()
 
-  fun <T> fromJson(json: String, clazz: Class<T>): T? = try {
-    moshi.adapter(clazz).fromJson(json)
+  inline fun <reified T> fromJson(@Language("JSON") string: String): T? = try {
+    moshi.adapter(T::class.java).fromJson(string)
   } catch (e: Exception) {
     e.printStackTrace()
     null
   }
 
-  fun <T> fromJson(json: String, typeOfT: Type): T? = try {
-    moshi.adapter<T>(typeOfT).fromJson(json)
+  inline fun <reified T> fromJson(
+    @Language("JSON") string: String,
+    rawType: Class<*>,
+    vararg typeArguments: Class<*>
+  ): T? = try {
+    moshi.adapter<T>(Types.newParameterizedType(rawType, *typeArguments)).fromJson(string)
   } catch (e: Exception) {
     e.printStackTrace()
     null
   }
 
-  fun <T> fromJson(json: String, rawType: Class<*>, vararg typeArguments: Class<*>): T? = try {
-    fromJson(json, Types.newParameterizedType(rawType, *typeArguments))
+  @Language("JSON")
+  inline fun <reified T> toJson(value: T?): String? = try {
+    moshi.adapter(T::class.java).toJson(value)
   } catch (e: Exception) {
     e.printStackTrace()
     null
   }
-
-  fun <T> toJson(o: T?, clazz: Class<T>): String? = try {
-    moshi.adapter(clazz).toJson(o)
-  } catch (e: Exception) {
-    e.printStackTrace()
-    null
-  }
-
-  inline fun <reified T> toJson(o: T?): String? = toJson(o, T::class.java)
 }
 
-inline fun <reified T> String.fromJson(): T? = JsonUtil.fromJson(this, T::class.java)
-
-inline fun <reified T> String.fromJson(typeOfT: Type): T? = JsonUtil.fromJson(this, typeOfT)
+inline fun <reified T> String.fromJson(): T? = JsonUtil.fromJson(this)
 
 inline fun <reified T> String.fromJson(rawType: Class<*>, vararg typeArguments: Class<*>): T? =
   JsonUtil.fromJson(this, rawType, *typeArguments)
 
+@Language("JSON")
 fun Any?.toJson(): String? = JsonUtil.toJson(this)
