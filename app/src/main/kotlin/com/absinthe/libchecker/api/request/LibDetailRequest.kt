f0618d32914b658ccf3bda0508f8bb8bbd8d493a
liuzho <qq354295878@gmail.com>
2022-03-26 10:13:52 +0800

    add 'Referer' header for 'requestLibDetail' request
    gitee raw data required this header, otherwise return empty content

--- app/src/main/kotlin/com/absinthe/libchecker/api/request/LibDetailRequest.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/api/request/LibDetailRequest.kt
@@ -1,13 +1,16 @@
 package com.absinthe.libchecker.api.request
 
+import com.absinthe.libchecker.BuildConfig
 import com.absinthe.libchecker.api.bean.LibDetailBean
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 
 interface LibDetailRequest {
   @GET("{categoryDir}/{libName}.json")
   suspend fun requestLibDetail(
     @Path("categoryDir") categoryDir: String,
-    @Path("libName") libName: String
+    @Path("libName") libName: String,
+    @Header("Referer") referer: String = BuildConfig.APPLICATION_ID,
   ): LibDetailBean
 }
