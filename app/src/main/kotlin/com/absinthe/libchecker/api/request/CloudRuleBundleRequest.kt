d241c8b7b63fd58c78edafaaed818245aa197beb
Absinthe <zhaobozhen2025@gmail.com>
2022-03-26 13:54:05 +0800

    Add 'Referer' header for 'requestCloudRuleInfo' request

--- app/src/main/kotlin/com/absinthe/libchecker/api/request/CloudRuleBundleRequest.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/api/request/CloudRuleBundleRequest.kt
@@ -1,11 +1,13 @@
 package com.absinthe.libchecker.api.request
 
+import com.absinthe.libchecker.BuildConfig
 import com.absinthe.libchecker.api.bean.CloudRuleInfo
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 const val VERSION = 3
 
 interface CloudRuleBundleRequest {
   @GET("cloud/md5/v$VERSION")
-  suspend fun requestCloudRuleInfo(): CloudRuleInfo?
+  suspend fun requestCloudRuleInfo(@Header("Referer") referer: String = BuildConfig.APPLICATION_ID): CloudRuleInfo?
 }
