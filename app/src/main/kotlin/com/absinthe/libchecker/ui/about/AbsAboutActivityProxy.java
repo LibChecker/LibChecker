486f098eed27f6fb2bf55833fd38e547f683fdd3
qhy040404 <qhy040404@163.com>
2022-07-27 17:05:54 +0800

    Apply rikka's material v2.5.1 and fix NullPointerException

--- app/src/main/kotlin/com/absinthe/libchecker/ui/about/AbsAboutActivityProxy.java
+++ app/src/main/kotlin/com/absinthe/libchecker/ui/about/AbsAboutActivityProxy.java
@@ -256,11 +256,6 @@ public abstract class AbsAboutActivityProxy extends MaterialActivity {
     return super.onOptionsItemSelected(menuItem);
   }
 
-  @Override
-  public void setTitle(@NonNull CharSequence title) {
-    collapsingToolbar.setTitle(title);
-  }
-
   public Toolbar getToolbar() {
     return toolbar;
   }
