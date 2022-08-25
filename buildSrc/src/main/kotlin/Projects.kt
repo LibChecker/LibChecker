1544e6f5837a3d4f897a797299dff85d311dd0a7
Absinthe <zhaobozhen2025@gmail.com>
2022-08-25 13:58:06 +0800

    Prepare for 3.2.1.

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -9,7 +9,7 @@ import java.io.File
 import java.nio.charset.Charset
 import java.time.Instant
 
-const val baseVersionName = "2.3.0"
+const val baseVersionName = "2.3.1"
 val verName: String by lazy { "${baseVersionName}${versionNameSuffix}.${"git rev-parse --short HEAD".exec()}" }
 val verCode: Int by lazy { "git rev-list --count HEAD".exec().toInt() }
 val isDevVersion: Boolean by lazy { "git tag -l $baseVersionName".exec().isEmpty() }
