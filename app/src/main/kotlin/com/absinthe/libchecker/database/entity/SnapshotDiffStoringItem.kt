33f1ec3006bc7f969f3bfe52c585a7acc9f6790f
Goooler <wangzongler@gmail.com>
2022-08-28 01:03:43 +0800

    Remove @ColumnInfo on fields that have the same names

--- app/src/main/kotlin/com/absinthe/libchecker/database/entity/SnapshotDiffStoringItem.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/database/entity/SnapshotDiffStoringItem.kt
@@ -1,12 +1,11 @@
 package com.absinthe.libchecker.database.entity
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "diff_table")
 data class SnapshotDiffStoringItem(
   @PrimaryKey val packageName: String,
-  @ColumnInfo val lastUpdatedTime: Long,
-  @ColumnInfo val diffContent: String
+  val lastUpdatedTime: Long,
+  val diffContent: String
 )
