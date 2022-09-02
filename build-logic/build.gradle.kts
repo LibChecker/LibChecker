cc31737de9951a299da922cc48872c1c66e7957f
Goooler <wangzongler@gmail.com>
2022-09-02 21:31:57 +0800

    Add an empty BuildLogic plugin

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,6 +7,15 @@ repositories {
   mavenCentral()
 }
 
+gradlePlugin {
+  plugins {
+    create("build-logic") {
+      id = "build-logic"
+      implementationClass = "BuildLogic"
+    }
+  }
+}
+
 dependencies {
   implementation(libs.gradlePlugin.android)
   implementation(libs.gradlePlugin.kotlin)
