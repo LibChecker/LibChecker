c0c3bc7c661fe45cc44d5c6ab0202764652e0b7e
qhy040404 <45379733+qhy040404@users.noreply.github.com>
2022-10-09 09:39:50 +0800

    [skip ci] Update protobuf (#450)
    * Fix build error
    
    * Exactly import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,5 @@
 import com.android.build.gradle.internal.api.ApkVariantOutputImpl
-import com.google.protobuf.gradle.builtins
-import com.google.protobuf.gradle.generateProtoTasks
 import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
 import java.nio.file.Paths
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
 
