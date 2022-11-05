72ca37f34dbf47a86da2b1b28c01f2a510b03fe3
Absinthe <zhaobozhen2025@gmail.com>
2022-11-05 21:56:00 +0800

    Fix `Called attach on a child which is not detached`.

--- app/src/main/kotlin/com/absinthe/libchecker/ui/fragment/statistics/LibReferenceFragment.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/ui/fragment/statistics/LibReferenceFragment.kt
@@ -10,9 +10,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.widget.Space
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -167,14 +165,6 @@ class LibReferenceFragment :
           layoutParams = FrameLayout.LayoutParams(
             FrameLayout.LayoutParams.MATCH_PARENT,
             FrameLayout.LayoutParams.MATCH_PARENT
-          )
-        }
-      )
-      setFooterView(
-        Space(context).apply {
-          layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            96.dp
           )
         }
       )
