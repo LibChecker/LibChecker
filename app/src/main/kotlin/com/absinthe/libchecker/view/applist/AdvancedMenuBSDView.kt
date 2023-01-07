740d7db665129b02a9064d05066ac535edcb95a3
qhy040404 <qhy040404@163.com>
2023-01-07 17:38:01 +0800

    Let options scrollable

--- app/src/main/kotlin/com/absinthe/libchecker/view/applist/AdvancedMenuBSDView.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/view/applist/AdvancedMenuBSDView.kt
@@ -15,8 +15,10 @@ import com.absinthe.libchecker.bean.LibStringItem
 import com.absinthe.libchecker.bean.LibStringItemChip
 import com.absinthe.libchecker.constant.Constants
 import com.absinthe.libchecker.database.entity.LCItem
+import com.absinthe.libchecker.recyclerview.BottomSheetRecyclerView
 import com.absinthe.libchecker.recyclerview.adapter.AppAdapter
 import com.absinthe.libchecker.recyclerview.adapter.detail.LibStringAdapter
+import com.absinthe.libchecker.recyclerview.adapter.main.AdvancedMenuAdapter
 import com.absinthe.libchecker.utils.extensions.dp
 import com.absinthe.libchecker.utils.extensions.getColorByAttr
 import com.absinthe.libchecker.utils.extensions.getColorStateListByAttr
@@ -35,6 +37,8 @@ class AdvancedMenuBSDView(context: Context) : LinearLayout(context), IHeaderView
       LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
     title.text = context.getString(R.string.advanced_menu)
   }
+
+  private val adapter = AdvancedMenuAdapter()
 
   private val demoAdapter = AppAdapter().also {
     it.cardMode = AppAdapter.CardMode.DEMO
@@ -161,6 +165,29 @@ class AdvancedMenuBSDView(context: Context) : LinearLayout(context), IHeaderView
     flexDirection = FlexDirection.ROW
   }
 
+  private val list = BottomSheetRecyclerView(context).apply {
+    layoutParams = LayoutParams(
+      ViewGroup.LayoutParams.MATCH_PARENT,
+      ViewGroup.LayoutParams.WRAP_CONTENT
+    )
+    overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+    adapter = this@AdvancedMenuBSDView.adapter
+    layoutManager = LinearLayoutManager(context)
+    isVerticalScrollBarEnabled = false
+    clipToPadding = false
+    clipChildren = false
+    isNestedScrollingEnabled = true
+    setHasFixedSize(true)
+
+    this@AdvancedMenuBSDView.adapter.apply {
+      addData(demoView)
+      addData(sortView)
+      addData(flexLayout)
+      addData(itemView)
+      addData(itemFlexLayout)
+    }
+  }
+
   fun addOptionItemView(labelRes: Int, option: Int): AdvancedMenuItemView {
     val view = AdvancedMenuItemView(context).apply {
       setOption(labelRes, option)
@@ -192,11 +219,7 @@ class AdvancedMenuBSDView(context: Context) : LinearLayout(context), IHeaderView
     val padding = 16.dp
     setPadding(padding, padding, padding, 0)
     addView(header)
-    addView(demoView)
-    addView(sortView)
-    addView(flexLayout)
-    addView(itemView)
-    addView(itemFlexLayout)
+    addView(list)
   }
 
   override fun getHeaderView(): BottomSheetHeaderView {
