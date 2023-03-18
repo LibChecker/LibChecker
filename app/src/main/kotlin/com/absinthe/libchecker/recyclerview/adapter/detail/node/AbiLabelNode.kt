e81383ecef6e5a49485826060aabca1de615750c
Absinthe <zhaobozhen2025@gmail.com>
2023-03-18 19:50:17 +0800

    New detail abi labels.

---
+++ app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/detail/node/AbiLabelNode.kt
@@ -0,0 +1,4 @@
+package com.absinthe.libchecker.recyclerview.adapter.detail.node
+
+class AbiLabelNode(val abi: Int, val active: Boolean) : BaseAbiLabelsNode() {
+}
--- app/src/main/kotlin/com/absinthe/libchecker/view/detail/DetailsTitleView.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/view/detail/DetailsTitleView.kt
@@ -1,9 +1,6 @@
 package com.absinthe.libchecker.view.detail
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.Typeface
-import android.text.format.Formatter
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
@@ -12,10 +9,13 @@ import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.marginStart
 import androidx.core.view.marginTop
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.absinthe.libchecker.R
+import com.absinthe.libchecker.recyclerview.HorizontalSpacesItemDecoration
+import com.absinthe.libchecker.recyclerview.adapter.detail.AbiLabelsAdapter
 import com.absinthe.libchecker.utils.extensions.getColor
 import com.absinthe.libchecker.utils.extensions.getColorByAttr
-import com.absinthe.libchecker.utils.extensions.getColorStateListByAttr
 import com.absinthe.libchecker.utils.extensions.getDimensionPixelSize
 import com.absinthe.libchecker.view.AViewGroup
 import com.absinthe.libchecker.view.app.AlwaysMarqueeTextView
@@ -26,26 +26,6 @@ class DetailsTitleView(context: Context, attributeSet: AttributeSet? = null) :
   val iconView = AppCompatImageView(context).apply {
     val iconSize = context.getDimensionPixelSize(R.dimen.lib_detail_icon_size)
     layoutParams = LayoutParams(iconSize, iconSize)
-    addView(this)
-  }
-
-  private val appSizeView = AppCompatTextView(
-    ContextThemeWrapper(
-      context,
-      R.style.TextView_SansSerifCondensedMedium
-    )
-  ).apply {
-    layoutParams = LayoutParams(
-      ViewGroup.LayoutParams.WRAP_CONTENT,
-      ViewGroup.LayoutParams.WRAP_CONTENT
-    ).also {
-      it.topMargin = 4.dp
-    }
-    setPadding(4.dp, 2.dp, 4.dp, 2.dp)
-    setBackgroundResource(R.drawable.bg_app_size_label)
-    setTextColor(android.R.color.darker_gray.getColor(context))
-    setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
-    setTypeface(null, Typeface.BOLD)
     addView(this)
   }
 
@@ -107,28 +87,31 @@ class DetailsTitleView(context: Context, attributeSet: AttributeSet? = null) :
     addView(this)
   }
 
-  fun setAppSize(size: Long) {
-    val tint: ColorStateList = if (size <= 20 * MEGA_BYTE_SI_UNITS) {
-      context.getColorStateList(R.color.material_green_700)
-    } else if (size <= 50 * MEGA_BYTE_SI_UNITS) {
-      context.getColorStateList(R.color.material_blue_700)
-    } else if (size <= 100 * MEGA_BYTE_SI_UNITS) {
-      context.getColorStateList(R.color.material_yellow_700)
-    } else if (size <= 150 * MEGA_BYTE_SI_UNITS) {
-      context.getColorStateList(R.color.material_red_700)
-    } else {
-      context.getColorStateListByAttr(com.google.android.material.R.attr.colorOnSurface)
-    }
-    appSizeView.backgroundTintList = tint
-    appSizeView.setTextColor(tint)
+  val abiLabelsAdapter = AbiLabelsAdapter()
 
-    appSizeView.text = Formatter.formatFileSize(context, size)
+  val abiLabelsRecyclerView = RecyclerView(context).apply {
+    layoutParams = LayoutParams(
+      ViewGroup.LayoutParams.WRAP_CONTENT,
+      ViewGroup.LayoutParams.WRAP_CONTENT
+    ).also {
+      it.topMargin = 4.dp
+      it.marginStart = (-4).dp
+    }
+    overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+    adapter = abiLabelsAdapter
+    layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    isHorizontalScrollBarEnabled = false
+    clipToPadding = false
+    clipChildren = false
+    isNestedScrollingEnabled = false
+    setHasFixedSize(true)
+    addItemDecoration(HorizontalSpacesItemDecoration(4.dp))
+    this@DetailsTitleView.addView(this)
   }
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
     super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     iconView.autoMeasure()
-    appSizeView.autoMeasure()
     val textWidth =
       measuredWidth - paddingStart - paddingEnd - iconView.measuredWidth - appNameView.marginStart
     appNameView.measure(
@@ -147,23 +130,32 @@ class DetailsTitleView(context: Context, attributeSet: AttributeSet? = null) :
       textWidth.toExactlyMeasureSpec(),
       extraInfoView.defaultHeightMeasureSpec(this)
     )
+    abiLabelsRecyclerView.measure(
+      textWidth.toExactlyMeasureSpec(),
+      abiLabelsRecyclerView.defaultHeightMeasureSpec(this)
+    )
     val basicInfoTotalHeight =
-      appNameView.measuredHeight + packageNameView.measuredHeight + versionInfoView.measuredHeight + extraInfoView.measuredHeight
+      appNameView.measuredHeight +
+        packageNameView.measuredHeight +
+        versionInfoView.measuredHeight +
+        extraInfoView.measuredHeight +
+        abiLabelsRecyclerView.measuredHeight +
+        abiLabelsRecyclerView.marginTop
     setMeasuredDimension(
       measuredWidth,
       paddingTop +
-        (basicInfoTotalHeight).coerceAtLeast(iconView.measuredHeight + appSizeView.marginTop + appSizeView.measuredHeight) +
+        (basicInfoTotalHeight).coerceAtLeast(iconView.measuredHeight) +
         paddingBottom
     )
   }
 
   override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
     iconView.layout(paddingStart, paddingTop)
-    appSizeView.layout(iconView.left + (iconView.measuredWidth - appSizeView.measuredWidth) / 2, iconView.bottom + appSizeView.marginTop)
     appNameView.layout(iconView.right + appNameView.marginStart, paddingTop)
     packageNameView.layout(appNameView.left, appNameView.bottom)
     versionInfoView.layout(appNameView.left, packageNameView.bottom)
     extraInfoView.layout(appNameView.left, versionInfoView.bottom)
+    abiLabelsRecyclerView.layout(appNameView.left + abiLabelsRecyclerView.marginStart, extraInfoView.bottom + abiLabelsRecyclerView.marginTop)
   }
 
   companion object {
