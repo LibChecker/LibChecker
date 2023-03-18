17534f62165fcf1d5ca444a6a189493b0a247050
Absinthe <zhaobozhen2025@gmail.com>
2023-03-18 19:52:52 +0800

    Lint.

--- app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/detail/node/AbiLabelNode.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/detail/node/AbiLabelNode.kt
@@ -1,4 +1,3 @@
 package com.absinthe.libchecker.recyclerview.adapter.detail.node
 
-class AbiLabelNode(val abi: Int, val active: Boolean) : BaseAbiLabelsNode() {
-}
+class AbiLabelNode(val abi: Int, val active: Boolean) : BaseAbiLabelsNode()
