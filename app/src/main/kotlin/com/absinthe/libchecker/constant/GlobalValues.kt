4a84d82b5d09baf3dfed9f492375f0cf658c1ca3
Absinthe <zhaobozhen2025@gmail.com>
2023-05-11 23:42:17 +0800

    Lint.

--- app/src/main/kotlin/com/absinthe/libchecker/constant/GlobalValues.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/constant/GlobalValues.kt
@@ -4,7 +4,6 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import androidx.lifecycle.MutableLiveData
 import com.absinthe.libchecker.BuildConfig
-import com.absinthe.libchecker.annotation.NATIVE
 import com.absinthe.libchecker.ui.fragment.detail.MODE_SORT_BY_SIZE
 import com.absinthe.libchecker.utils.LCAppUtils
 import com.absinthe.libchecker.utils.SPDelegates
