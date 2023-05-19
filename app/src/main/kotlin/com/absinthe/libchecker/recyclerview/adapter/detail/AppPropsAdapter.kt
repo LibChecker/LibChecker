36ef03d5a3e8ed963e622a8e102a2f38c072e7c4
qhy040404 <qhy040404@163.com>
2023-05-19 12:19:18 +0800

    Apply suggestion

--- app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/detail/AppPropsAdapter.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/recyclerview/adapter/detail/AppPropsAdapter.kt
@@ -12,6 +12,7 @@ import com.absinthe.libchecker.model.AppPropItem
 import com.absinthe.libchecker.ui.fragment.detail.EXTRA_TEXT
 import com.absinthe.libchecker.ui.fragment.detail.XmlBSDFragment
 import com.absinthe.libchecker.utils.extensions.maybeResourceId
+import com.absinthe.libchecker.utils.manifest.PropertiesMap
 import com.absinthe.libchecker.utils.manifest.ResourceParser
 import com.absinthe.libchecker.view.detail.AppPropItemView
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -43,145 +44,15 @@ class AppPropsAdapter(
     }
   }
 
-  private val category = mapOf(
-    0 to "game",
-    1 to "audio",
-    2 to "video",
-    3 to "image",
-    4 to "social",
-    5 to "news",
-    6 to "maps",
-    7 to "productivity",
-    8 to "accessibility"
-  )
-
-  private val installLocation = mapOf(
-    0 to "auto",
-    1 to "internalOnly",
-    2 to "preferExternal"
-  )
-
-  private val configs = mapOf(
-    0x0001 to "mcc",
-    0x0002 to "mnc",
-    0x0004 to "locale",
-    0x0008 to "touchscreen",
-    0x0010 to "keyboard",
-    0x0020 to "keyboardHidden",
-    0x0040 to "navigation",
-    0x0080 to "orientation",
-    0x0100 to "screenLayout",
-    0x0200 to "uiMode",
-    0x0400 to "screenSize",
-    0x0800 to "smallestScreenSize",
-    0x1000 to "density",
-    0x2000 to "layoutDirection",
-    0x4000 to "colorMode",
-    0x8000 to "grammaticalGender",
-    0x10000000 to "fontWeightAdjustment",
-    0x40000000 to "fontScale"
-  )
-
-  private val screenOrientations = mapOf(
-    -1 to "unspecified",
-    0 to "landscape",
-    1 to "portrait",
-    2 to "user",
-    3 to "behind",
-    4 to "sensor",
-    5 to "nosensor",
-    6 to "sensorLandscape",
-    7 to "sensorPortrait",
-    8 to "reverseLandscape",
-    9 to "reversePortrait",
-    10 to "fullSensor",
-    11 to "userLandscape",
-    12 to "userPortrait",
-    13 to "fullUser",
-    14 to "locked"
-  )
-
-  private val windowSoftInputModes = mapOf(
-    0x30 to "adjustNothing",
-    0x20 to "adjustPan",
-    0x10 to "adjustResize",
-    5 to "stateAlwaysVisible",
-    4 to "stateVisible",
-    3 to "stateAlwaysHidden",
-    2 to "stateHidden",
-    1 to "stateUnchanged"
-  )
-
-  private val gwpAsanModes = mapOf(
-    -1 to "default",
-    0 to "never",
-    1 to "always"
-  )
-
-  private val uiOptions = mapOf(
-    0 to "none",
-    1 to "splitActionBarWhenNarrow"
-  )
-
-  private val launchModes = mapOf(
-    0 to "standard",
-    1 to "singleTop",
-    2 to "singleTask",
-    3 to "singleInstance",
-    4 to "singleInstance"
-  )
-
   private val linkable = setOf("string", "array", "bool", "xml", "drawable", "mipmap", "color", "dimen")
 
   private fun parseValue(item: AppPropItem): String {
-    return when {
-      item.key == "appCategory" -> {
-        category.getValue(item.value.toInt())
-      }
-      item.key == "installLocation" -> {
-        installLocation.getValue(item.value.toInt())
-      }
-      item.key == "configChanges" -> {
-        buildString {
-          configs.forEach { (code, name) ->
-            if (code and item.value.toInt() > 0) {
-              append("|$name")
-            }
-          }
-        }.substring(1)
-      }
-      item.key == "screenOrientation" -> {
-        screenOrientations.getValue(item.value.toInt())
-      }
-      item.key == "windowSoftInputMode" -> {
-        buildString {
-          var value = item.value.toInt()
-          windowSoftInputModes.forEach { (code, name) ->
-            if (value - code >= 0) {
-              append("|$name")
-              value -= code
-            }
-          }
-          if (this.isEmpty()) {
-            append("|stateUnspecified|adjustUnspecified")
-          }
-        }.substring(1)
-      }
-      item.key == "gwpAsanMode" -> {
-        gwpAsanModes.getValue(item.value.toInt())
-      }
-      item.key == "uiOptions" -> {
-        uiOptions.getValue(item.value.toInt())
-      }
-      item.key == "launchMode" -> {
-        launchModes.getValue(item.value.toInt())
-      }
-      item.value.maybeResourceId() -> {
-        runCatching {
-          appResources.getResourceName(item.value.toInt())
-        }.getOrDefault(item.value)
-      }
-      else -> item.value
+    return if (item.value.maybeResourceId()) {
+      runCatching {
+        appResources.getResourceName(item.value.toInt())
+      }.getOrDefault(PropertiesMap.parseProperty(item.key, item.value))
+    } else {
+      PropertiesMap.parseProperty(item.key, item.value)
     }
   }
 
