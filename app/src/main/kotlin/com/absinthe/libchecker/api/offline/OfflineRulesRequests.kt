32946e0c2d22e2c0eb89566c6c54bffb2c0fa0e3
qhy040404 <qhy040404@163.com>
2023-06-26 16:00:24 +0800

    Fix wrong count

--- app/src/main/kotlin/com/absinthe/libchecker/api/offline/OfflineRulesRequests.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/api/offline/OfflineRulesRequests.kt
@@ -5,18 +5,24 @@ import com.absinthe.libchecker.api.bean.CloudRuleInfo
 import com.absinthe.libchecker.api.bean.LibDetailBean
 import com.absinthe.libchecker.utils.fromJson
 import java.io.File
-import java.io.FileFilter
 
 object OfflineRulesRequests {
   private val rules = File(LibCheckerApp.app.filesDir, "rules/LibChecker-Rules-master")
 
   fun getVersion(): Int {
+    check(rules.exists())
     return File(rules, "cloud/md5/v3").readText().fromJson<CloudRuleInfo>()!!.version
   }
 
   fun getCount(): Int {
-    return rules.listFiles(FileFilter { it.absolutePath.contains("libs") })!!
-      .sumOf { it.listFiles()!!.size }
+    return rules.listFiles { file -> file.absolutePath.contains("libs") }?.sumOf { libs ->
+      libs.listFiles()?.toMutableList()?.apply {
+        this.filter { it.isDirectory }.forEach {
+          remove(it)
+          addAll(it.listFiles() ?: arrayOf())
+        }
+      }?.size ?: 0
+    } ?: 0
   }
 
   fun getLibDetail(categoryDir: String, libName: String): LibDetailBean {
