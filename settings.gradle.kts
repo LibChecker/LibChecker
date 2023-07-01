f1940b7d05122c079d83d2b6455647f0cdfdcb30
Goooler <wangzongler@gmail.com>
2023-07-01 09:40:43 +0800

    Simple property assignment in Kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
   repositories {
     google()
     mavenCentral()
