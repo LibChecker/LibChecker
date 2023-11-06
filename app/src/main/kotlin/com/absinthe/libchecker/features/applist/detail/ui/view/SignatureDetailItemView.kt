e3ad2e3f36dc21229a9339e877776492e0a9ec29
qhy040404 <qhy040404@163.com>
2023-11-06 20:39:30 +0800

    #979

--- app/src/main/kotlin/com/absinthe/libchecker/features/applist/detail/ui/view/SignatureDetailItemView.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/features/applist/detail/ui/view/SignatureDetailItemView.kt
@@ -2,22 +2,23 @@ package com.absinthe.libchecker.features.applist.detail.ui.view
 
 import android.content.Context
 import android.util.TypedValue
-import android.view.Gravity
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatTextView
-import com.absinthe.libchecker.utils.extensions.dp
+import androidx.core.view.marginStart
+import com.absinthe.libchecker.compat.VersionCompat
 import com.absinthe.libchecker.utils.extensions.getColorByAttr
+import com.absinthe.libchecker.utils.extensions.getDrawableByAttr
+import com.absinthe.libchecker.view.AViewGroup
+import rikka.core.util.ClipboardUtils
 
-class SignatureDetailItemView(context: Context) : LinearLayout(context) {
+class SignatureDetailItemView(context: Context) : AViewGroup(context) {
 
   val type = AppCompatTextView(context).apply {
     layoutParams = LayoutParams(
       ViewGroup.LayoutParams.MATCH_PARENT,
       ViewGroup.LayoutParams.WRAP_CONTENT
-    ).also {
-      it.gravity = Gravity.START
-    }
+    )
     setTextColor(context.getColorByAttr(com.google.android.material.R.attr.colorOnSurface))
     setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
   }
@@ -28,7 +29,23 @@ class SignatureDetailItemView(context: Context) : LinearLayout(context) {
       ViewGroup.LayoutParams.WRAP_CONTENT
     ).also {
       it.topMargin = 4.dp
-      it.gravity = Gravity.START
+    }
+  }
+
+  private val copyToClipboard = AppCompatImageButton(context).apply {
+    layoutParams = LayoutParams(24.dp, 24.dp).also {
+      it.marginStart = 8.dp
+    }
+    scaleX = 0.8F
+    scaleY = 0.8F
+    setImageResource(com.google.android.material.R.drawable.abc_ic_menu_copy_mtrl_am_alpha)
+    setBackgroundDrawable(context.getDrawableByAttr(com.google.android.material.R.attr.selectableItemBackgroundBorderless))
+    setOnClickListener {
+      ClipboardUtils.put(
+        context,
+        "${type.text}:${content.text}"
+      )
+      VersionCompat.showCopiedOnClipboardToast(context)
     }
   }
 
@@ -37,9 +54,37 @@ class SignatureDetailItemView(context: Context) : LinearLayout(context) {
       ViewGroup.LayoutParams.MATCH_PARENT,
       ViewGroup.LayoutParams.WRAP_CONTENT
     )
-    orientation = VERTICAL
     setPadding(8.dp, 2.dp, 8.dp, 2.dp)
     addView(type)
+    addView(copyToClipboard)
     addView(content)
+  }
+
+  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    copyToClipboard.autoMeasure()
+    val typeWidth =
+      measuredWidth - paddingStart - paddingEnd - copyToClipboard.measuredWidth - copyToClipboard.marginStart
+    val contentWidth = measuredWidth - paddingStart - paddingEnd
+    type.measure(
+      typeWidth.toExactlyMeasureSpec(),
+      copyToClipboard.measuredHeight.toExactlyMeasureSpec()
+    )
+    content.measure(
+      contentWidth.toExactlyMeasureSpec(),
+      content.defaultHeightMeasureSpec(this)
+    )
+    setMeasuredDimension(
+      measuredWidth,
+      (type.measuredHeight + content.measuredHeight + paddingTop + paddingBottom).coerceAtLeast(
+        40.dp
+      )
+    )
+  }
+
+  override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+    type.layout(paddingStart, paddingTop)
+    content.layout(type.left, type.bottom)
+    copyToClipboard.layout(paddingEnd, copyToClipboard.toViewVerticalCenter(type), true)
   }
 }
