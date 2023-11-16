0b4eae700fc724d707d4b82e1471d3b7e818ec76
Absinthe <me@absinthe.life>
2023-11-16 09:56:25 +0800

    Refactor chart. (#989)
    * Refactor chart.
    
    Part of #329
    
    * Apply suggestions from code review
    
    Co-authored-by: Goooler <wangzongler@gmail.com>
    
    ---------
    
    Co-authored-by: Goooler <wangzongler@gmail.com>

--- app/src/main/kotlin/com/absinthe/libchecker/features/chart/ChartViewModel.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/features/chart/ChartViewModel.kt
@@ -1,14 +1,53 @@
 package com.absinthe.libchecker.features.chart
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.absinthe.libchecker.database.Repositories
 import com.absinthe.libchecker.database.entity.LCItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ChartViewModel : ViewModel() {
   val dbItems: LiveData<List<LCItem>> = Repositories.lcRepository.allDatabaseItems
   val filteredList: MutableLiveData<List<LCItem>> = MutableLiveData()
   val dialogTitle: MutableLiveData<String> = MutableLiveData()
   val androidVersion: MutableLiveData<Triple<Int, String, Int?>?> = MutableLiveData()
+  private var queryJob: Job? = null
+
+  private val _isLoading = MutableStateFlow(false)
+  val isLoading = _isLoading.asStateFlow()
+
+  fun setLoading(loading: Boolean) {
+    _isLoading.value = loading
+  }
+
+  fun <T : View> applyChartData(
+    root: ViewGroup,
+    currentChartView: View?,
+    newChartView: T,
+    source: IChartDataSource<T>
+  ) {
+    queryJob?.cancel()
+    queryJob = viewModelScope.launch(Dispatchers.Default) {
+      source.fillChartView(newChartView)
+
+      withContext(Dispatchers.Main) {
+        if (currentChartView != null) {
+          root.removeView(currentChartView)
+        }
+        root.addView(newChartView)
+        if (dbItems.value?.isNotEmpty() == true) {
+          setLoading(false)
+        }
+      }
+    }
+  }
 }
