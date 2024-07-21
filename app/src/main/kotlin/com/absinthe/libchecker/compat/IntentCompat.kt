8d655bf97970e75342ee4f7579ce5126443ce98a
Shen Lin <shhenlin@gmail.com>
2024-07-21 09:46:51 +0800

    Migrate getParcelableExtra and getSerializableExtra to androidx impl (#1279)

--- app/src/main/kotlin/com/absinthe/libchecker/compat/IntentCompat.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/compat/IntentCompat.kt
@@ -5,7 +5,6 @@ package com.absinthe.libchecker.compat
 import android.content.Intent
 import android.os.Parcelable
 import androidx.core.content.IntentCompat as AndroidXIntentCompat
-import com.absinthe.libchecker.utils.OsUtils
 import java.io.Serializable
 
 object IntentCompat {
@@ -14,10 +13,6 @@ object IntentCompat {
   }
 
   inline fun <reified T : Serializable> getSerializableExtra(intent: Intent, name: String): T? {
-    return if (OsUtils.atLeastT()) {
-      intent.getSerializableExtra(name, T::class.java)
-    } else {
-      intent.getSerializableExtra(name) as T?
-    }
+    return AndroidXIntentCompat.getSerializableExtra(intent, name, T::class.java)
   }
 }
