19e3048c17517fd3d55de2f89810026af85fb2c1
Absinthe <zhaobozhen2025@gmail.com>
2024-08-04 15:52:15 +0800

    Optimize blur behind animation of dialog.

--- app/src/main/kotlin/com/absinthe/libchecker/ui/base/BaseAlertDialogBuilder.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/ui/base/BaseAlertDialogBuilder.kt
@@ -1,5 +1,6 @@
 package com.absinthe.libchecker.ui.base
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
@@ -16,7 +17,27 @@ class BaseAlertDialogBuilder : MaterialAlertDialogBuilder {
         dialog.window?.let {
           it.addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND)
           it.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
-          it.attributes.blurBehindRadius = 64
+
+          if (OsUtils.atLeastT()) {
+            val animator = ValueAnimator.ofInt(0, 13).apply {
+              duration = 350
+              addUpdateListener { animation ->
+                if (!it.decorView.isAttachedToWindow) {
+                  cancel()
+                  return@addUpdateListener
+                }
+                val blurRadius = animation.animatedValue as Int * 5
+                it.attributes.blurBehindRadius = blurRadius
+                it.attributes = it.attributes
+              }
+            }
+
+            dialog.setOnShowListener {
+              animator.start()
+            }
+          } else {
+            it.attributes.blurBehindRadius = 64
+          }
         }
       }
     }
