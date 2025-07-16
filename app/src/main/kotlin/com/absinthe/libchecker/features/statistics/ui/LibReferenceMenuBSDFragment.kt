10860fba94c9fcd8ce4622d8c5621bea545f978a
Absinthe <zhaobozhen2025@gmail.com>
2025-07-16 20:43:49 +0800

    Adjust order.

--- app/src/main/kotlin/com/absinthe/libchecker/features/statistics/ui/LibReferenceMenuBSDFragment.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/features/statistics/ui/LibReferenceMenuBSDFragment.kt
@@ -27,12 +27,12 @@ class LibReferenceMenuBSDFragment : BaseBottomSheetViewDialogFragment<LibReferen
     optionsViewMap[LibReferenceOptions.ACTIVITIES] = root.addOptionItemView(R.string.ref_category_activity, LibReferenceOptions.ACTIVITIES)
     optionsViewMap[LibReferenceOptions.RECEIVERS] = root.addOptionItemView(R.string.ref_category_br, LibReferenceOptions.RECEIVERS)
     optionsViewMap[LibReferenceOptions.PROVIDERS] = root.addOptionItemView(R.string.ref_category_cp, LibReferenceOptions.PROVIDERS)
+    optionsViewMap[LibReferenceOptions.ACTION] = root.addOptionItemView(R.string.ref_category_action, LibReferenceOptions.ACTION)
     optionsViewMap[LibReferenceOptions.PERMISSIONS] = root.addOptionItemView(R.string.ref_category_perm, LibReferenceOptions.PERMISSIONS)
     optionsViewMap[LibReferenceOptions.METADATA] = root.addOptionItemView(R.string.ref_category_metadata, LibReferenceOptions.METADATA)
     optionsViewMap[LibReferenceOptions.PACKAGES] = root.addOptionItemView(R.string.ref_category_package, LibReferenceOptions.PACKAGES)
     optionsViewMap[LibReferenceOptions.SHARED_UID] = root.addOptionItemView(R.string.ref_category_shared_uid, LibReferenceOptions.SHARED_UID)
     optionsViewMap[LibReferenceOptions.ONLY_NOT_MARKED] = root.addOptionItemView(R.string.ref_category_only_not_marked, LibReferenceOptions.ONLY_NOT_MARKED)
-    optionsViewMap[LibReferenceOptions.ACTION] = root.addOptionItemView(R.string.ref_category_action, LibReferenceOptions.ACTION)
 
     dialog?.setOnDismissListener {
       onDismissCallback(previousAdvancedOptions.xor(GlobalValues.libReferenceOptions))
