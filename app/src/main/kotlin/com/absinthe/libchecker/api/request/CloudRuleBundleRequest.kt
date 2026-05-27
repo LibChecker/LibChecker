b8f47f4ea253f72ed6ebbf001f573c5e9d3a4a5a
Absinthe <me@absinthe.life>
2026-05-27 22:36:47 +0800

    Remove BaseUrlInterceptor and inline endpoints.

--- app/src/main/kotlin/com/absinthe/libchecker/api/request/CloudRuleBundleRequest.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/api/request/CloudRuleBundleRequest.kt
@@ -2,27 +2,23 @@ package com.absinthe.libchecker.api.request
 
 import com.absinthe.libchecker.BuildConfig
 import com.absinthe.libchecker.api.ApiManager
-import com.absinthe.libchecker.api.HEADER_BASE_URL
 import com.absinthe.libchecker.api.bean.CloudRuleInfo
 import com.absinthe.libchecker.api.bean.RepoInfoResp
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.Path
 
 const val VERSION = 4
-
-const val REPO_INFO = "Repo-Info"
-const val OWNER = "Owner"
-const val REPO = "Repo"
 
 interface CloudRuleBundleRequest {
   @GET("cloud/md5/v$VERSION")
   suspend fun requestCloudRuleInfo(@Header("Referer") referer: String = BuildConfig.APPLICATION_ID): CloudRuleInfo?
 
-  @Headers("Accept: application/vnd.github.v3+json", "$HEADER_BASE_URL: $REPO_INFO")
+  @Headers("Accept: application/vnd.github.v3+json")
   @GET(ApiManager.GITHUB_API_REPO_INFO)
   suspend fun requestRepoInfo(
-    @Header(OWNER) owner: String,
-    @Header(REPO) repo: String
+    @Path("owner") owner: String,
+    @Path("repo") repo: String
   ): RepoInfoResp?
 }
