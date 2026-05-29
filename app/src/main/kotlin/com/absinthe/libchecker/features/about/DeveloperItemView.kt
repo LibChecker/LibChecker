b2a101bcf4b21f2a35ca55afaa244bf0dd855725
KRLHY <1428915180@qq.com>
2026-05-29 14:10:02 +0800

    Add CardBackground for developer

--- app/src/main/kotlin/com/absinthe/libchecker/features/about/DeveloperItemView.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/features/about/DeveloperItemView.kt
@@ -11,6 +11,7 @@ import androidx.core.view.marginRight
 import com.absinthe.libchecker.R
 import com.absinthe.libchecker.utils.extensions.dp
 import com.absinthe.libchecker.utils.extensions.getColorByAttr
+import com.absinthe.libchecker.utils.extensions.getColorStateListByAttr
 import com.absinthe.libchecker.utils.extensions.getDimensionPixelSize
 import com.absinthe.libchecker.utils.extensions.getResourceIdByAttr
 import com.absinthe.libchecker.utils.extensions.setSmoothRoundCorner
@@ -27,6 +28,7 @@ class DeveloperItemView(context: Context) : MaterialCardView(context) {
   init {
     setSmoothRoundCorner(16.dp)
     strokeColor = context.getColorByAttr(com.google.android.material.R.attr.colorOutlineVariant)
+    setCardBackgroundColor(context.getColorStateListByAttr(com.google.android.material.R.attr.colorSurfaceContainerHigh))
     addView(container)
   }
 
