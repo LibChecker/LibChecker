1b974a3340d15d3c9a4a08970c1bde69034ac908
Absinthe <zhaobozhen2025@gmail.com>
2026-06-05 10:43:53 +0800

    Replace adapter footer spacing with padding.

--- app/src/main/kotlin/com/absinthe/libchecker/utils/extensions/AdapterExtensions.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/utils/extensions/AdapterExtensions.kt
@@ -1,41 +1,15 @@
 package com.absinthe.libchecker.utils.extensions
 
-import android.view.ViewGroup
-import android.widget.Space
 import androidx.recyclerview.widget.RecyclerView
 import com.absinthe.libchecker.R
 import com.chad.library.adapter.base.HeaderFooterSupport
 
 fun HeaderFooterSupport.setSpaceFooterView() {
-  legacyRecyclerViewOrNull?.let { rv ->
-    fun should(): Boolean {
-      val a = rv.childCount
-      val b = legacyItemCount()
-      return if (!hasFooterLayout()) {
-        a >= b
-      } else {
-        a >= b - 1
-      }
-    }
-    if (should()) {
-      if (hasFooterLayout()) return
-      Space(rv.context).apply {
-        layoutParams = ViewGroup.LayoutParams(
-          ViewGroup.LayoutParams.MATCH_PARENT,
-          96.dp
-        )
-      }.also {
-        setFooterView(it)
-      }
-    } else {
-      if (!hasFooterLayout()) return
-      removeAllFooterView()
-    }
-  }
+  legacyRecyclerViewOrNull?.setBottomPaddingSpace()
 }
 
 fun RecyclerView.setBottomPaddingSpace() {
-  val addedPadding = getTag(R.id.adapter_bottom_padding_id)?.toString().orEmpty().isNotBlank()
+  val addedPadding = getTag(R.id.adapter_bottom_padding_id) == true
   fun should(): Boolean {
     val a = childCount
     val b = adapter?.itemCount ?: 0
